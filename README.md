deda97456178e906b1b0e866ccbaad71c72cf016
Zacharias <alienfromdia@proton.me>
2026-06-18 16:04:09 +0200

    Initial commit of sligtly modified GTNH based 1.7.10 mod

---
+++ README.md
@@ -0,0 +1,7 @@
+# Speedometer 1.7.10 GTNH port
+
+This backport to 1.7.10 is intended for GTNH, might work in normal 1.7.10 aswell but i dont know and dont plan to test it
+
+This is a fork/clone of [Example Mod 1.7.10](https://github.com/GTNewHorizons/ExampleMod1.7.10)
+
+Original [README](https://github.com/zaze06/Speedometer/blob/GTNH-port/README-org.md)
---
+++ src/main/java/me/zacharias/speedometer/Speedometer.java
@@ -0,0 +1,46 @@
+package me.zacharias.speedometer;
+
+import com.myname.mymodid.MyMod;
+import com.myname.mymodid.Tags;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.SidedProxy;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.event.FMLServerStartingEvent;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = Speedometer.MODID, version = Tags.VERSION, name = "Speedometer", acceptedMinecraftVersions = "[1.7.10]")
+public class Speedometer {
+    public static final String MODID = "speedometer";
+    public static final Logger LOG = LogManager.getLogger(MODID);
+
+    @SidedProxy(clientSide = "me.zacharias.speedometer.ClientProxy", serverSide = "me.zacharias.speedometer.CommonProxy")
+    public static CommonProxy proxy;
+
+    @Mod.EventHandler
+    // preInit "Run before anything else. Read your config, create blocks, items, etc, and register them with the
+    // GameRegistry." (Remove if not needed)
+    public void preInit(FMLPreInitializationEvent event) {
+        proxy.preInit(event);
+    }
+
+    @Mod.EventHandler
+    // load "Do your mod setup. Build whatever data structures you care about. Register recipes." (Remove if not needed)
+    public void init(FMLInitializationEvent event) {
+        proxy.init(event);
+    }
+
+    @Mod.EventHandler
+    // postInit "Handle interaction with other mods, complete your setup based on this." (Remove if not needed)
+    public void postInit(FMLPostInitializationEvent event) {
+        proxy.postInit(event);
+    }
+
+    @Mod.EventHandler
+    // register server commands in this event handler (Remove if not needed)
+    public void serverStarting(FMLServerStartingEvent event) {
+        proxy.serverStarting(event);
+    }
+}
